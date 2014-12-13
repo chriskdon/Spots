fe100e35df08dea7178c620f9f8ec37184c61ae8
kubasub <kubasub12@gmail.com>
2014-12-13 18:03:35 -0500

    changed tab size to 2 spaces

--- prototyping/Kuba/DotsCanvas/app/src/main/java/ca/brocku/dotscanvas/app/MainMenuActivity.java
+++ prototyping/Kuba/DotsCanvas/app/src/main/java/ca/brocku/dotscanvas/app/MainMenuActivity.java
@@ -10,62 +10,62 @@ import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
 public class MainMenuActivity extends ActionBarActivity {
-    FrameLayout dim_overlay;
+  FrameLayout dim_overlay;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main_menu);
 
-        ImageButton lifeButton = (ImageButton) findViewById(R.id.btn_StartLifeGame);
-        dim_overlay = (FrameLayout) findViewById(R.id.dim_overlay);
+    ImageButton lifeButton = (ImageButton) findViewById(R.id.btn_StartLifeGame);
+    dim_overlay = (FrameLayout) findViewById(R.id.dim_overlay);
 
-        lifeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            MainMenuActivity.this.startActivity(new Intent(MainMenuActivity.this, MainActivity.class));
-            }
-        });
+    lifeButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        MainMenuActivity.this.startActivity(new Intent(MainMenuActivity.this, MainActivity.class));
+      }
+    });
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+    if (id == R.id.action_settings) {
+      return true;
     }
+    return super.onOptionsItemSelected(item);
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
+  @Override
+  protected void onResume() {
+    dimActivity(false);
+    super.onResume();
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+  @Override
+  public void startActivity(Intent intent) {
+    dimActivity(true);
+    super.startActivity(intent);
+  }
 
-    @Override
-    protected void onResume() {
-        dimActivity(false);
-        super.onResume();
-    }
-
-    @Override
-    public void startActivity(Intent intent) {
-        dimActivity(true);
-        super.startActivity(intent);
-    }
-
-    /**
-     * Toggle the overlay for the activity.
-     * <p/>
-     * Used when starting/returning from other activities.
-     */
-    private void dimActivity(boolean shouldDim) {
-        int visibility = shouldDim ? View.VISIBLE : View.INVISIBLE;
-        dim_overlay.setVisibility(visibility);
-    }
+  /**
+   * Toggle the overlay for the activity.
+   * <p/>
+   * Used when starting/returning from other activities.
+   */
+  private void dimActivity(boolean shouldDim) {
+    int visibility = shouldDim ? View.VISIBLE : View.INVISIBLE;
+    dim_overlay.setVisibility(visibility);
+  }
 }
--- prototyping/Kuba/DotsCanvas/app/src/main/java/ca/brocku/dotscanvas/app/engine/Handlers/ScoreViewHandler.java
+++ prototyping/Kuba/DotsCanvas/app/src/main/java/ca/brocku/dotscanvas/app/engine/Handlers/ScoreViewHandler.java
@@ -5,34 +5,31 @@ import android.os.Handler;
 import android.os.Message;
 import android.widget.TextView;
 
-/**
- * Author: Chris Kellendonk
- * Student #: 4810800
- * Date: 2014-06-17
- */
 public class ScoreViewHandler extends Handler {
-    private static final String KEY_GAME_SCORE = "GAME_SCORE";
+  private static final String KEY_GAME_SCORE = "GAME_SCORE";
 
-    private TextView textView;
+  private TextView textView;
 
-    public ScoreViewHandler(TextView textView) {
-        if(textView == null) { throw new IllegalArgumentException("[textView] can't be null."); }
-
-        this.textView = textView;
+  public ScoreViewHandler(TextView textView) {
+    if (textView == null) {
+      throw new IllegalArgumentException("[textView] can't be null.");
     }
 
-    public void updateScore(int score) {
-        // Update score and send message to myself
-        Message message = obtainMessage();
-        Bundle bundle = new Bundle();
-        bundle.putInt(KEY_GAME_SCORE, score);
-        message.setData(bundle);
-        sendMessage(message);
-    }
+    this.textView = textView;
+  }
 
-    @Override
-    public void handleMessage(Message msg) {
-        String score = String.valueOf(msg.getData().getInt(KEY_GAME_SCORE));
-        textView.setText(score);
-    }
+  public void updateScore(int score) {
+    // Update score and send message to myself
+    Message message = obtainMessage();
+    Bundle bundle = new Bundle();
+    bundle.putInt(KEY_GAME_SCORE, score);
+    message.setData(bundle);
+    sendMessage(message);
+  }
+
+  @Override
+  public void handleMessage(Message msg) {
+    String score = String.valueOf(msg.getData().getInt(KEY_GAME_SCORE));
+    textView.setText(score);
+  }
 }
